101b8999011e5993adedf258417597b7a39369c8
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-03-19 20:32:45 +0300

    ShiftLeft DONE

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -5,10 +5,15 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+#define tab "\t"
+
 //#define ARRAY
+//#define CLASSWORK
 //#define ARRAY_1
 //#define ARRAY_2
+//#define ARAAY_3
 //#define ARRAY_4
+//#define ARRAY_5
 //#define ARRAY_6
 
 void main()
@@ -29,6 +34,45 @@ void main()
 	cout << endl;
 #endif // ARRAY
 
+#ifdef CLASSWORK
+	//Вывод элементов массива с клавиатуры
+	cout << "Введите элементы массива:";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cin >> arr[i];
+	}
+	//Вывод массива на экран в прямом порядке:
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+	//Вывод массива на экран в обратном порядке:
+	for (int i = SIZE - 1; i >= 0; i--)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+	//Вычисление суммы элементов массива:
+	int sum = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Сумма элементов массива:" << sum << endl;
+	cout << "Среднее-арифметическое:" << (double)sum / SIZE << endl;
+	//Поиск минимального и максимального значения в массиве:
+	int min, max;
+	min = max = arr[0];
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)max = arr[i];
+	}
+	cout << "Минимальное значение в массиве:" << min << endl;
+	cout << "Максимальное значение в массиве:" << max << endl;
+#endif // CLASSWORK
+
 #ifdef ARRAY_1
 	
 	for (int i = 0; i < SIZE; i++)
@@ -46,24 +90,39 @@ void main()
 	cout << endl;
 #endif // ARRAY_2
 
+#ifdef ARRAY_3
+	for (int i = SIZE - 1; i >= 0; i--)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+#endif // ARRAY_3
+
 #ifdef ARRAY_4
 	int sum = 0;
 	for (int i = 0; i < SIZE; i++)
 	{
 		sum += arr[i];
-		cout << "Сумма элементов массива:" << sum << endl;
 	}
+		cout << "Сумма элементов массива:" << sum << endl;
 #endif // ARRAY_4
+
+#ifdef ARRAY_5
+	int sum = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Среднее-арифметическое:" << (double)sum / SIZE << endl;
+#endif // ARRAY_5
 
 #ifdef ARRAY_6
 	int max = arr[0];
 	int min = arr[0];
 	for (int i = 0; i < SIZE; i++)
 	{
-		if (min > arr[i])
-			min = arr[i];
-		if (max < arr[i])
-			max = arr[i];
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)max = arr[i];
 	}
 	cout << "Минимальное значение:" << min << endl;
 	cout << "Максимальное значение:" << max << endl;
