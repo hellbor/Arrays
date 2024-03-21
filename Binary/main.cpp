8b468daa7508625d83fe53fc3f2f73c3de101788
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-03-21 21:25:30 +0300

    Binary DONE

--- Binary/main.cpp
+++ Binary/main.cpp
@@ -4,5 +4,19 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-
+	int decimal;
+	cout << "Введите десятичное число:"; cin >> decimal;
+	const int MAX_CAPACITY = 32;
+	bool binary[MAX_CAPACITY]{};
+	int i = 0;
+	for (; decimal > 0; i++)
+	{
+		binary[i] = decimal % 2;
+		decimal /= 2;
+	}
+	for (--i; i >= 0; i--)
+	{
+		cout << binary[i];
+	}
+	cout << endl;
 }
--- Shift/main.cpp
+++ Shift/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define tab "\t"
 //#define SHIFT_LEFT
 //#define SHIFT_RIGHT
+//#define SHIFT_RIGHT_CLASSWORK
 
 void main()
 {
@@ -13,7 +14,7 @@ void main()
 	//int() - значение по умолчанию для типа 'double'
 	const int n = 10;
 	int arr[n] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
-	
+
 	//Вывод исходного массива на экран:
 	for (int i = 0; i < n; i++)
 	{
@@ -36,7 +37,7 @@ void main()
 		arr[n - 1] = buffer;
 	}
 #endif // SHIFT_LEFT
-	
+
 #ifdef SHIFT_RIGHT
 	for (int i = 0; i < n - number_of_shifts; i++)
 	{
@@ -48,7 +49,20 @@ void main()
 		arr[n - 1] = buffer;
 	}
 #endif // SHIFT_RIGHT
-	
+
+#ifdef SHIFT_RIGHT_CLASSWORK
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[n - 1];
+		for (int i = n - 1; i > 0; i--)
+		{
+			arr[i] = arr[i - 1];
+		}
+		arr[0] = buffer;
+	}
+
+#endif // SHIFT_RIGHT_CLASSWORK
+
 	//Вывод сдвинутого массива на экран:
 	for (int i = 0; i < n; i++)
 	{
