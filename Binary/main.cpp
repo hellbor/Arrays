94c03b589fc74302b09a47703f34b85eeba5ec9c
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-03-28 18:51:03 +0300

    Binary DONE

--- Binary/main.cpp
+++ Binary/main.cpp
@@ -1,11 +1,16 @@
 ﻿#include<iostream>
 using namespace std;
 
+//#define BINARY
+#define HEX
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	int decimal;
 	cout << "Введите десятичное число:"; cin >> decimal;
+
+#ifdef BINARY
 	const int MAX_CAPACITY = 32;
 	bool binary[MAX_CAPACITY]{};
 	int i = 0;
@@ -19,4 +24,22 @@ void main()
 		cout << binary[i];
 	}
 	cout << endl;
+#endif // BINARY
+
+#ifdef HEX
+	const int MAX_HEX_CAPACITY = 8;
+	char hex[MAX_HEX_CAPACITY] = {};
+	int i = 0;
+	for (; decimal; i++)
+	{
+		hex[i] = decimal % 16;
+		decimal /= 16;
+	}
+	for (--i; i >= 0; i--)
+	{
+		cout << (hex[i] < 10 ? (char)hex[i] : char(hex[i] + 55));
+	}
+	cout << endl;
+#endif // HEX
+
 }
