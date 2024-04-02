c2643b8867cd5e08f7b87b09bf3b28524f75f214
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-04-02 17:55:06 +0300

    Unique DONE

--- Unique/main.cpp
+++ Unique/main.cpp
@@ -6,6 +6,8 @@ using std::endl;
 
 #define tab "\t"
 
+//#define UNIQUE_1
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -14,14 +16,27 @@ void main()
 	int minRand = 20;
 	int maxRand = 30;
 
-	
+#ifdef UNIQUE_1
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % (maxRand - minRand) + minRand;
 	}
-		for(int i = minRand; i <= maxRand; i++)
+	for (int i = minRand; i <= maxRand; i++)
 	{
-			cout << i << tab;
+		cout << i << tab;
 	}
 	cout << endl;
+#endif // UNIQUE_1
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+	
+
 }
