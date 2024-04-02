b984a5f105c43ca3a8e75e6a052281bb38dee269
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-04-02 15:16:55 +0300

    Random DONE

---
+++ Random/main.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	int minRand, maxRand;
+	cout << "¬ведите минимальное случайное число:"; cin >> minRand;
+	cout << "¬ведите максимальное случайное число:"; cin >> maxRand;
+	//заполнение массива случайными числами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+
+	//вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		if (minRand < maxRand)cout << arr[i] << tab;
+		else if (minRand > maxRand)cout << "ERROR" << endl;
+		else if (minRand == maxRand)cout << "ERROR" << endl;
+	}
+	while (minRand != maxRand && minRand<maxRand);
+
+	//сортировка массива
+	for (int i = 0; i < n; i++)//выбирает элемент в который нужно поместить
+	{
+		for (int j = i + 1; j < n; j++)//перебирает оставшиес€ элементы
+		{
+			//arr[i] - выбранный элемент
+			//arr[j] - перебираемый элемент
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+
+	//вывод отсортированного массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		if (minRand < maxRand)cout << arr[i] << tab;
+	}
+	cout << endl;
+}
---
+++ Unique/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 1;
+	int arr[n];
+	int minRand = 20;
+	int maxRand = 30;
+	
+	for (int i = 0; i < n; i++)
+	{
+		for(int i = minRand; i <= maxRand; i++)
+		cout << i << tab;
+	}
+	cout << endl;
+}
