c3f5ddcf69d8d408616355a5a5b0e033cdd87629
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-04-04 19:27:11 +0300

    Search DONE

---
+++ Search/main.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+using namespace std;
+
+#define tab "\t";
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	const int n = 10;
+	int arr[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 8;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	//Поиск повторений:
+	for (int i = 0; i < n; i++)
+	{
+		bool met_before = false;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				met_before = true;
+				break;
+			}
+		}
+		if (met_before)continue;
+		int counter = 0;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[i] == arr[j])counter++;
+		}
+		if (counter)cout << "Значение " << arr[i] << " повторяется " << counter << " раз" << endl;
+	}
+} 
